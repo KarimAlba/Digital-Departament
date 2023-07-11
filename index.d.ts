679f0e9679ceeaded4f40779fbfbb784c4562530
Karim <karim_skiy@mail.ru>
2023-07-11 17:06:34 +0300

    router rebased, favicon added

--- index.d.ts
+++ index.d.ts
@@ -5,3 +5,4 @@ declare module'*.scss' {
 
 declare module "*.svg";
 declare module "*.png";
+declare module "*.ico";
--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,18 +1,13 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import Autorization from '../components/views/Autorization';
-import Registration from '../components/views/Registration';
-import Main from '../components/views/MainPage';
+import Router from '../router';
+import Favicon from "react-favicon";
+import FavIconImg from '../assets/images/icons/favicon.ico';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Autorization/>}/>
-        <Route path='registration' element={<Registration/>}/>
-        <Route path='main/*' element={<Main/>}/>
-        <Route path='*' element={<h1>Сломався...</h1>}/>
-      </Routes>
+      <Favicon url={FavIconImg}/>
+      <Router/>
     </div>
   );
 }
